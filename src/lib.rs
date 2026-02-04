8e369ed5022cdc29366d1449714324064ee72b13
N4CH723HR3R <n4ch7@n3831.net>
2026-02-04 14:20:26 +0100

    add get_alphabet to get the actual alphabet of the enum members "Alphabet"

--- src/lib.rs
+++ src/lib.rs
@@ -19,24 +19,28 @@ pub enum Alphabet {
     Z,
 }
 
-const CROCKFORD: &'static [u8] = b"0123456789ABCDEFGHJKMNPQRSTVWXYZ";
-const RFC4648: &'static [u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
-const RFC4648_LOWER: &'static [u8] = b"abcdefghijklmnopqrstuvwxyz234567";
-const RFC4648_HEX: &'static [u8] = b"0123456789ABCDEFGHIJKLMNOPQRSTUV";
-const RFC4648_HEX_LOWER: &'static [u8] = b"0123456789abcdefghijklmnopqrstuv";
-const Z: &'static [u8] = b"ybndrfg8ejkmcpqxot1uwisza345h769";
+// constants always have static lifetimes
+const CROCKFORD: &[u8] = b"0123456789ABCDEFGHJKMNPQRSTVWXYZ";
+const RFC4648: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+const RFC4648_LOWER: &[u8] = b"abcdefghijklmnopqrstuvwxyz234567";
+const RFC4648_HEX: &[u8] = b"0123456789ABCDEFGHIJKLMNOPQRSTUV";
+const RFC4648_HEX_LOWER: &[u8] = b"0123456789abcdefghijklmnopqrstuv";
+const Z: &[u8] = b"ybndrfg8ejkmcpqxot1uwisza345h769";
 
-pub fn encode(alphabet: Alphabet, data: &[u8]) -> String {
-    let (alphabet, padding) = match alphabet {
+pub fn get_alphabet(alphabet: Alphabet) -> (&'static [u8], bool) {
+    match alphabet {
         Alphabet::Crockford => (CROCKFORD, false),
         Alphabet::Rfc4648 { padding } => (RFC4648, padding),
         Alphabet::Rfc4648Lower { padding } => (RFC4648_LOWER, padding),
         Alphabet::Rfc4648Hex { padding } => (RFC4648_HEX, padding),
         Alphabet::Rfc4648HexLower { padding } => (RFC4648_HEX_LOWER, padding),
         Alphabet::Z => (Z, false),
-    };
-    let mut ret = Vec::with_capacity((data.len() + 3) / 4 * 5);
+    }
+}
 
+pub fn encode(alphabet: Alphabet, data: &[u8]) -> String {
+    let mut ret = Vec::with_capacity(data.len().div_ceil(4) * 5);
+    let (alphabet, padding) = get_alphabet(alphabet);
     for chunk in data.chunks(5) {
         let buf = {
             let mut buf = [0u8; 5];
@@ -55,9 +59,9 @@ pub fn encode(alphabet: Alphabet, data: &[u8]) -> String {
         ret.push(alphabet[(buf[4] & 0x1F) as usize]);
     }
 
-    if data.len() % 5 != 0 {
+    if !data.len().is_multiple_of(5) {
         let len = ret.len();
-        let num_extra = 8 - (data.len() % 5 * 8 + 4) / 5;
+        let num_extra = 8 - (data.len() % 5 * 8).div_ceil(5);
         if padding {
             for i in 1..num_extra + 1 {
                 ret[len - i] = b'=';
@@ -78,64 +82,64 @@ pub fn encode(alphabet: Alphabet, data: &[u8]) -> String {
 */
 
 const CROCKFORD_INV: [i8; 75] = [
-     0,  1,  2,  3,  4,  5,  6,  7,  8,  9, -1, -1, -1, -1, -1, -1, -1, 10, 11, 12,
-    13, 14, 15, 16, 17,  1, 18, 19,  1, 20, 21,  0, 22, 23, 24, 25, 26, -1, 27, 28,
-    29, 30, 31, -1, -1, -1, -1, -1, -1, 10, 11, 12, 13, 14, 15, 16, 17,  1, 18, 19,
-     1, 20, 21,  0, 22, 23, 24, 25, 26, -1, 27, 28, 29, 30, 31,
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -1, -1, -1, -1, -1, -1, -1, 10, 11, 12, 13, 14, 15, 16, 17, 1,
+    18, 19, 1, 20, 21, 0, 22, 23, 24, 25, 26, -1, 27, 28, 29, 30, 31, -1, -1, -1, -1, -1, -1, 10,
+    11, 12, 13, 14, 15, 16, 17, 1, 18, 19, 1, 20, 21, 0, 22, 23, 24, 25, 26, -1, 27, 28, 29, 30,
+    31,
 ];
 const RFC4648_INV: [i8; 75] = [
-    -1, -1, 26, 27, 28, 29, 30, 31, -1, -1, -1, -1, -1, -1, -1, -1, -1,  0,  1,  2,
-     3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-    23, 24, 25, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, 26, 27, 28, 29, 30, 31, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8,
+    9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1,
 ];
 const RFC4648_INV_PAD: [i8; 75] = [
-    -1, -1, 26, 27, 28, 29, 30, 31, -1, -1, -1, -1, -1,  0, -1, -1, -1,  0,  1,  2,
-     3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-    23, 24, 25, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, 26, 27, 28, 29, 30, 31, -1, -1, -1, -1, -1, 0, -1, -1, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8,
+    9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1,
 ];
 const RFC4648_INV_LOWER: [i8; 75] = [
-    -1, -1, 26, 27, 28, 29, 30, 31, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10,
-    11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+    -1, -1, 26, 27, 28, 29, 30, 31, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+    25,
 ];
 const RFC4648_INV_LOWER_PAD: [i8; 75] = [
-    -1, -1, 26, 27, 28, 29, 30, 31, -1, -1, -1, -1, -1,  0, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10,
-    11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+    -1, -1, 26, 27, 28, 29, 30, 31, -1, -1, -1, -1, -1, 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+    25,
 ];
 const RFC4648_INV_HEX: [i8; 75] = [
-     0,  1,  2,  3,  4,  5,  6,  7,  8,  9, -1, -1, -1, -1, -1, -1, -1, 10, 11, 12,
-    13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -1, -1, -1, -1, -1, -1, -1, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+    19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1,
 ];
 const RFC4648_INV_HEX_PAD: [i8; 75] = [
-     0,  1,  2,  3,  4,  5,  6,  7,  8,  9, -1, -1, -1,  0, -1, -1, -1, 10, 11, 12,
-    13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -1, -1, -1, 0, -1, -1, -1, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+    19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1,
 ];
 const RFC4648_INV_HEX_LOWER: [i8; 75] = [
-     0,  1,  2,  3,  4,  5,  6,  7,  8,  9, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-    21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, -1, -1, -1, -1,
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 10,
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, -1, -1, -1,
+    -1,
 ];
 const RFC4648_INV_HEX_LOWER_PAD: [i8; 75] = [
-     0,  1,  2,  3,  4,  5,  6,  7,  8,  9, -1, -1, -1,  0, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-    21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, -1, -1, -1, -1,
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -1, -1, -1, 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 10,
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, -1, -1, -1,
+    -1,
 ];
 const Z_INV: [i8; 75] = [
-    -1, 18, -1, 25, 26, 27, 30, 29,  7, 31, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, 24,  1, 12,  3,  8,  5,  6, 28, 21,  9, 10,
-    -1, 11,  2, 16, 13, 14,  4, 22, 17, 19, -1, 20, 15,  0, 23,
+    -1, 18, -1, 25, 26, 27, 30, 29, 7, 31, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, 24, 1, 12, 3, 8, 5, 6, 28, 21, 9, 10, -1, 11, 2, 16, 13, 14, 4, 22, 17, 19, -1, 20, 15, 0,
+    23,
 ];
 
 pub fn decode(alphabet: Alphabet, data: &str) -> Option<Vec<u8>> {
@@ -145,10 +149,34 @@ pub fn decode(alphabet: Alphabet, data: &str) -> Option<Vec<u8>> {
     let data = data.as_bytes();
     let alphabet = match alphabet {
         Alphabet::Crockford => CROCKFORD_INV, // supports both upper and lower case
-        Alphabet::Rfc4648 { padding } => if padding { RFC4648_INV_PAD } else { RFC4648_INV }
-        Alphabet::Rfc4648Lower { padding } => if padding { RFC4648_INV_LOWER_PAD } else { RFC4648_INV_LOWER }
-        Alphabet::Rfc4648Hex { padding } => if padding { RFC4648_INV_HEX_PAD } else { RFC4648_INV_HEX }
-        Alphabet::Rfc4648HexLower { padding } => if padding { RFC4648_INV_HEX_LOWER_PAD } else { RFC4648_INV_HEX_LOWER }
+        Alphabet::Rfc4648 { padding } => {
+            if padding {
+                RFC4648_INV_PAD
+            } else {
+                RFC4648_INV
+            }
+        }
+        Alphabet::Rfc4648Lower { padding } => {
+            if padding {
+                RFC4648_INV_LOWER_PAD
+            } else {
+                RFC4648_INV_LOWER
+            }
+        }
+        Alphabet::Rfc4648Hex { padding } => {
+            if padding {
+                RFC4648_INV_HEX_PAD
+            } else {
+                RFC4648_INV_HEX
+            }
+        }
+        Alphabet::Rfc4648HexLower { padding } => {
+            if padding {
+                RFC4648_INV_HEX_LOWER_PAD
+            } else {
+                RFC4648_INV_HEX_LOWER
+            }
+        }
         Alphabet::Z => Z_INV,
     };
     let mut unpadded_data_length = data.len();
@@ -159,7 +187,7 @@ pub fn decode(alphabet: Alphabet, data: &str) -> Option<Vec<u8>> {
         unpadded_data_length -= 1;
     }
     let output_length = unpadded_data_length * 5 / 8;
-    let mut ret = Vec::with_capacity((output_length + 4) / 5 * 5);
+    let mut ret = Vec::with_capacity(output_length.div_ceil(5) * 5);
     for chunk in data.chunks(8) {
         let buf = {
             let mut buf = [0u8; 8];
@@ -184,6 +212,8 @@ pub fn decode(alphabet: Alphabet, data: &str) -> Option<Vec<u8>> {
 #[cfg(test)]
 #[allow(dead_code, unused_attributes)]
 mod test {
+    use crate::{get_alphabet, RFC4648};
+
     use super::Alphabet::{Crockford, Rfc4648, Rfc4648Hex, Rfc4648HexLower, Rfc4648Lower, Z};
     use super::{decode, encode};
     use alloc::string::String;
@@ -281,11 +311,17 @@ mod test {
     #[test]
     fn masks_rfc4648_lower() {
         assert_eq!(
-            encode(Rfc4648Lower { padding: false }, &[0xF8, 0x3E, 0x7F, 0x83, 0xE7]),
+            encode(
+                Rfc4648Lower { padding: false },
+                &[0xF8, 0x3E, 0x7F, 0x83, 0xE7]
+            ),
             "7a7h7a7h"
         );
         assert_eq!(
-            encode(Rfc4648Lower { padding: false }, &[0x77, 0xC1, 0xF7, 0x7C, 0x1F]),
+            encode(
+                Rfc4648Lower { padding: false },
+                &[0x77, 0xC1, 0xF7, 0x7C, 0x1F]
+            ),
             "o7a7o7a7"
         );
         assert_eq!(
@@ -305,11 +341,17 @@ mod test {
     #[test]
     fn masks_rfc4648_lower_pad() {
         assert_eq!(
-            encode(Rfc4648Lower { padding: true }, &[0xF8, 0x3E, 0x7F, 0x83, 0xE7]),
+            encode(
+                Rfc4648Lower { padding: true },
+                &[0xF8, 0x3E, 0x7F, 0x83, 0xE7]
+            ),
             "7a7h7a7h"
         );
         assert_eq!(
-            encode(Rfc4648Lower { padding: true }, &[0x77, 0xC1, 0xF7, 0x7C, 0x1F]),
+            encode(
+                Rfc4648Lower { padding: true },
+                &[0x77, 0xC1, 0xF7, 0x7C, 0x1F]
+            ),
             "o7a7o7a7"
         );
         assert_eq!(
@@ -329,11 +371,17 @@ mod test {
     #[test]
     fn masks_rfc4648_hex() {
         assert_eq!(
-            encode(Rfc4648Hex { padding: false }, &[0xF8, 0x3E, 0x7F, 0x83, 0xE7]),
+            encode(
+                Rfc4648Hex { padding: false },
+                &[0xF8, 0x3E, 0x7F, 0x83, 0xE7]
+            ),
             "V0V7V0V7"
         );
         assert_eq!(
-            encode(Rfc4648Hex { padding: false }, &[0x77, 0xC1, 0xF7, 0x7C, 0x1F]),
+            encode(
+                Rfc4648Hex { padding: false },
+                &[0x77, 0xC1, 0xF7, 0x7C, 0x1F]
+            ),
             "EV0VEV0V"
         );
         assert_eq!(
@@ -353,11 +401,17 @@ mod test {
     #[test]
     fn masks_rfc4648_hex_pad() {
         assert_eq!(
-            encode(Rfc4648Hex { padding: true }, &[0xF8, 0x3E, 0x7F, 0x83, 0xE7]),
+            encode(
+                Rfc4648Hex { padding: true },
+                &[0xF8, 0x3E, 0x7F, 0x83, 0xE7]
+            ),
             "V0V7V0V7"
         );
         assert_eq!(
-            encode(Rfc4648Hex { padding: true }, &[0x77, 0xC1, 0xF7, 0x7C, 0x1F]),
+            encode(
+                Rfc4648Hex { padding: true },
+                &[0x77, 0xC1, 0xF7, 0x7C, 0x1F]
+            ),
             "EV0VEV0V"
         );
         assert_eq!(
@@ -377,11 +431,17 @@ mod test {
     #[test]
     fn masks_rfc4648_hex_lower() {
         assert_eq!(
-            encode(Rfc4648HexLower { padding: false }, &[0xF8, 0x3E, 0x7F, 0x83, 0xE7]),
+            encode(
+                Rfc4648HexLower { padding: false },
+                &[0xF8, 0x3E, 0x7F, 0x83, 0xE7]
+            ),
             "v0v7v0v7"
         );
         assert_eq!(
-            encode(Rfc4648HexLower { padding: false }, &[0x77, 0xC1, 0xF7, 0x7C, 0x1F]),
+            encode(
+                Rfc4648HexLower { padding: false },
+                &[0x77, 0xC1, 0xF7, 0x7C, 0x1F]
+            ),
             "ev0vev0v"
         );
         assert_eq!(
@@ -393,7 +453,10 @@ mod test {
             [0xC1, 0xD4, 0x7C, 0x1D, 0x47]
         );
         assert_eq!(
-            encode(Rfc4648HexLower { padding: false }, &[0xF8, 0x3E, 0x7F, 0x83]),
+            encode(
+                Rfc4648HexLower { padding: false },
+                &[0xF8, 0x3E, 0x7F, 0x83]
+            ),
             "v0v7v0o"
         );
     }
@@ -401,11 +464,17 @@ mod test {
     #[test]
     fn masks_rfc4648_hex_lower_pad() {
         assert_eq!(
-            encode(Rfc4648HexLower { padding: true }, &[0xF8, 0x3E, 0x7F, 0x83, 0xE7]),
+            encode(
+                Rfc4648HexLower { padding: true },
+                &[0xF8, 0x3E, 0x7F, 0x83, 0xE7]
+            ),
             "v0v7v0v7"
         );
         assert_eq!(
-            encode(Rfc4648HexLower { padding: true }, &[0x77, 0xC1, 0xF7, 0x7C, 0x1F]),
+            encode(
+                Rfc4648HexLower { padding: true },
+                &[0x77, 0xC1, 0xF7, 0x7C, 0x1F]
+            ),
             "ev0vev0v"
         );
         assert_eq!(
@@ -424,14 +493,8 @@ mod test {
 
     #[test]
     fn masks_z() {
-        assert_eq!(
-            encode(Z, &[0xF8, 0x3E, 0x0F, 0x83, 0xE0]),
-            "9y9y9y9y"
-        );
-        assert_eq!(
-            encode(Z, &[0x07, 0xC1, 0xF0, 0x7C, 0x1F]),
-            "y9y9y9y9"
-        );
+        assert_eq!(encode(Z, &[0xF8, 0x3E, 0x0F, 0x83, 0xE0]), "9y9y9y9y");
+        assert_eq!(encode(Z, &[0x07, 0xC1, 0xF0, 0x7C, 0x1F]), "y9y9y9y9");
         assert_eq!(
             decode(Z, "9y9y9y9y").unwrap(),
             [0xF8, 0x3E, 0x0F, 0x83, 0xE0]
@@ -522,6 +585,11 @@ mod test {
     #[test]
     fn invalid_chars_unpadded_rfc4648() {
         assert_eq!(decode(Rfc4648 { padding: false }, ","), None)
+    }
+
+    #[test]
+    fn get_alphabets() {
+        assert_eq!(get_alphabet(Rfc4648 { padding: true }), (RFC4648, true))
     }
 }
 
